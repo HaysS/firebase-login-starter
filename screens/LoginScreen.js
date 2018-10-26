912fc81af5492b44be9ae15f401a91f06f8881cc
HaysS <stanfordhays@gmail.com>
2018-10-26 18:18:19 -0500

    updated LoginScreen

---
+++ screens/LoginScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+export default class LinksScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Links',
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+});
